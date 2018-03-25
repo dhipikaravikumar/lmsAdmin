02f38fc523c67fb516e1583f40e219c71ef0057c
unknown <deepikaravi1312@gmail.com>
2018-03-25 15:55:12 -0400

    first

---
+++ src/main/java/com/gcit/springboot/lms/SpringbootLmsAdminApplication.java
@@ -0,0 +1,29 @@
+/*package com.gcit.springboot.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLmsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLmsAdminApplication.class, args);
+	}
+}
+*/
+
+package com.gcit.springboot.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLmsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLmsAdminApplication.class, args);
+		System.out.println("Hello Admin!");
+	}
+}
+
+
