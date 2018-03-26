46a253531212b1a74abc1631268ac4a1a5ecea32
unknown <deepikaravi1312@gmail.com>
2018-03-26 15:13:25 -0400

    add

--- src/main/java/com/gcit/springboot/lms/SpringbootLmsAdminApplication.java
+++ src/main/java/com/gcit/springboot/lms/SpringbootLmsAdminApplication.java
@@ -19,7 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootLmsAdminApplication {
-
+	//comment
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootLmsAdminApplication.class, args);
 		System.out.println("Hello Admin!");
